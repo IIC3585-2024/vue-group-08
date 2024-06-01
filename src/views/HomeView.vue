6d0deb48f1fa124a67f861e19509c065cf4c6135
Vicente Cruz <vcrb@uc.cl>
2024-06-01 19:50:48 -0400

    feat - HomeView integrated with backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,72 +1,73 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import BookCoverImage from "../components/BookCoverImage.vue";
 
-// type Book = {
-//   id: string,
-//   name: string,
-//   introduction: string,
-//   author: string,
-// };
+const apiUrl = "http://localhost:3000";
 
-// const books: Book[] = [
-const books = [
-  {
-    id: "9780132350884",
-    name: "Clean Code",
-    introduction:
-      "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees.",
-    author: "Robert C. Martin",
-  },
-  {
-    id: "9780201485677",
-    name: "Refactoring",
-    introduction:
-      "As the application of object technology--particularly the Java programming language--has become commonplace, a new problem has emerged to confront the software development community.",
-    author: "Martin Fowler",
-  },
-  {
-    id: "9780131103627",
-    name: "The C Programming Language",
-    introduction:
-      "This book is meant to help the reader learn how to program in C.",
-    author: "Brian W. Kernighan, Dennis M. Ritchie",
-  },
-  {
-    id: "9780201633610",
-    name: "Design Patterns",
-    introduction:
-      "Capturing a wealth of experience about the design of object-oriented software, four top-notch researchers present a catalog of simple and succinct solutions to commonly occurring design problems.",
-    author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
-  },
-  {
-    id: "9780321125217",
-    name: "Domain-Driven Design",
-    introduction:
-      "Explains how to incorporate effective domain modeling into the software development process.",
-    author: "Eric Evans",
-  },
-  // Add more books as needed
-];
+const fetchedBooks = ref([]);
+const searchQuery = ref("");
+const isLoading = ref(true); // Variable para rastrear el estado de carga
+
+async function fetchBooks() {
+  isLoading.value = true; // Comienza la carga
+  try {
+    const response = await fetch(
+      `${apiUrl}/books/searchByTitle?title=Clean Code&limit=10&page=1&sortedBy=rating`
+    );
+    const data = await response.json();
+    fetchedBooks.value = data.books;
+  } catch (error) {
+    console.error("Error fetching books:", error);
+  } finally {
+    isLoading.value = false; // Termina la carga
+  }
+}
+
+onMounted(fetchBooks);
 </script>
 
 <template>
   <div class="container mx-auto px-4 py-6 mt-24">
     <div
+      v-if="isLoading"
+      role="status"
+      class="flex items-center justify-center"
+    >
+      <svg
+        aria-hidden="true"
+        class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span class="sr-only">Loading...</span>
+    </div>
+
+    <div
+      v-else
       class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
     >
       <div
-        v-for="book in books"
+        v-for="book in fetchedBooks"
         :key="book.id"
         class="border border-gray-300 p-4 rounded-lg flex flex-col items-center bg-white shadow-md dark:bg-gray-800 dark:border-gray-600 dark:shadow-lg"
       >
-        <BookCoverImage :src="book.id" :alt="book.name" size="M" />
         <div
           class="flex flex-col items-start mt-2 w-full text-gray-800 dark:text-gray-300"
         >
-          <div class="font-bold">{{ book.name }}</div>
-          <div class="mt-1">{{ book.introduction }}</div>
+          <div class="font-bold">{{ book.title }}</div>
+          <div class="mt-1">{{ book.first_sentence }}</div>
           <div class="mt-2 italic text-gray-500 dark:text-gray-400">
-            Author: {{ book.author }}
+            Author: {{ book.author_name }}
           </div>
         </div>
       </div>
