8bd65aa4d5a23408ea47bf1e5b79d2061a022ea6
Vicente Cruz <vcrb@uc.cl>
2024-06-01 23:07:18 -0400

    backup - implementing book details vue without being able to show the content of BookView

---
+++ src/views/BookView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex-1 justify-end items-end">
+    <h1>BookView</h1>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { defineProps } from "vue";
-import BookCoverImage from "../components/BookCoverImage.vue";
+import { useRouter } from "vue-router";
 import LoadingIcon from "../components/LoadingIcon.vue";
 
 const props = defineProps({
@@ -13,6 +13,14 @@ const props = defineProps({
     required: true,
   },
 });
+
+const router = useRouter();
+
+function handleClick(bookKey) {
+  const bookId = bookKey.split("/").pop();
+  // Navegar a la ruta dinámica /:bookKey
+  router.push({ name: "book", params: { bookKey: bookId } });
+}
 </script>
 
 <template>
@@ -43,6 +51,19 @@ const props = defineProps({
           <div class="mt-2 italic text-gray-500 dark:text-gray-400">
             Author: {{ book.author_name }}
           </div>
+          <button
+            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex self-end mt-4"
+            @click="handleClick(book.key)"
+          >
+            <svg
+              class="fill-current w-4 h-4 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+            </svg>
+            <span>Ver Libro</span>
+          </button>
         </div>
       </div>
     </div>
